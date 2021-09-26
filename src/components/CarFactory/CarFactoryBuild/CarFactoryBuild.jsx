7eaeb8395ced1997bb02732f60512ac2fd60a445
Dewald Els <dewaldifels@gmail.com>
2021-09-26 23:07:59 +0200

    FEAT: Display building message

--- src/components/CarFactory/CarFactoryBuild/CarFactoryBuild.jsx
+++ src/components/CarFactory/CarFactoryBuild/CarFactoryBuild.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react"
 import useStore from "../../../store/store"
 import styles from './CarFactoryBuild.module.css'
 
 const CarFactoryBuild = () => {
+    const [ building, setBuilding ] = useState(false)
 
     const carFactorySections = useStore(state => state.carFactorySections)
     let readyToBuild = carFactorySections.every(section => section.selectedValue !== null)
 
+    const handleClick = () => {
+        const buildTime = Math.random() * 10000
+        setBuilding(true)
+        setTimeout(() => {
+            setBuilding(false)
+        }, buildTime)
+    }
+
     return (
         <>
-            {readyToBuild &&
+            { readyToBuild &&
                 <section className={styles.CarFactoryBuild}>
                     <h2>That's all we need</h2>
                     <p>Let's build your car</p>
-                    <button className={styles.CarFactoryBuildButton}>Build your car</button>
+                    <button onClick={ handleClick } 
+                            disabled={ building }
+                            className={styles.CarFactoryBuildButton}>
+                                Build your car
+                    </button>
+                    { building &&
+                        <p>Where's busy building your car...</p>
+                    }
                 </section>
             }
         </>
